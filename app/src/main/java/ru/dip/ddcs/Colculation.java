6b352f39067ed28892f28a039e57d86bd918ef8e
Kirilyuk Vasily <Kirilyuk_94@inbox.ru>
2017-09-23 17:14:41 +0700

    добавлен класс

--- app/src/main/java/ru/dip/ddcs/Colculation.java
+++ app/src/main/java/ru/dip/ddcs/Colculation.java
@@ -3,11 +3,11 @@ package ru.dip.ddcs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Server extends AppCompatActivity {
+public class Colculation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_server);
+        setContentView(R.layout.activity_colculation);
     }
 }
